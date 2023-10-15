a0a03487a3cc4341a80371adaaf964602cdc4ab3
techakhil-me <techakhilc47@gmail.com>
2023-10-15 13:34:46 +0530

    url update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-const title = "Dharmik Wear";
+const title = "Dharmik Wear - Wear your beliefs";
 const desc =
   "Elevate your everyday look with the grace of Hindu gods on your chest.";
 const keywords = "Tshirt, hindu, sanaatan, dharmik, wear, fashion";
@@ -19,36 +19,36 @@ export default function Document() {
         <meta content="#282828" name="msapplication-TileColor" />
 
         <link
-          href="/favicons/apple-touch-icon.png"
+          href="/favicons/icon.png"
           rel="apple-touch-icon"
           sizes="180x180"
         />
         <link
-          href="/favicons/favicon-32x32.png"
+          href="/favicons/icon.png"
           rel="icon"
           sizes="32x32"
           type="image/png"
         />
         <link
-          href="/favicons/favicon-16x16.png"
+          href="/favicons/icon.png"
           rel="icon"
           sizes="16x16"
           type="image/png"
         />
-        <link href="/favicons/favicon.ico" rel="shortcut icon" />
+        <link href="/favicons/icon.png" rel="shortcut icon" />
         <link href="/favicons/site.webmanifest" rel="manifest" />
 
-        <meta property="og:url" content="https://haru-fashion.vercel.app" />
-        <link rel="canonical" href="https://haru-fashion.vercel.app" />
+        <meta property="og:url" content="https://www.dharmikwear.com/" />
+        <link rel="canonical" href="https://www.dharmikwear.com/" />
         <meta property="og:site_name" content="Dharmik Wear" />
         <meta property="og:description" content={desc} key="og_description" />
         <meta property="og:title" content={title} key="og_title" />
         <meta
           property="og:image"
-          content="https://haru-fashion.vercel.app/og.png"
+          content="https://www.dharmikwear.com/bg-img/banner_1.png"
         />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@dharmikwear" />
+        <meta name="twitter:site" content="@dharmik_wear" />
         <meta name="twitter:title" content={title} key="twitter_title" />
         <meta
           name="twitter:description"
@@ -57,7 +57,7 @@ export default function Document() {
         />
         <meta
           name="twitter:image"
-          content="https://haru-fashion.vercel.app/og.png"
+          content="https://www.dharmikwear.com/bg-img/banner_1.png"
         />
 
         <meta name="apple-mobile-web-app-capable" content="yes" />
